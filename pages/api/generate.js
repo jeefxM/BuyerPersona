f64487fb3b3f0820b245afdf5cd39980a8c7956c
unknown <jeefx123@gmail.com>
2023-02-06 12:11:20 +0400

    first

---
+++ pages/api/generate.js
@@ -0,0 +1,60 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
+    });
+    return;
+  }
+
+  const animal = req.body.animal || "";
+  if (animal.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      },
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(animal),
+      max_tokens: 1000,
+      temperature: 0.2,
+      top_p: 1,
+      stream: false,
+      logprobs: null,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
+}
+
+function generatePrompt(person) {
+  const capitalizedPerson =
+    person[0].toUpperCase() + person.slice(1).toLowerCase();
+  return `I am trying to write a buyer persona bio, please help me with writing ${capitalizedPerson}.`;
+}
